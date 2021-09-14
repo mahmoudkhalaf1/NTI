8b2ab13cb7f82f0f6757b3fe09f1c5411f0315fc
mahmoudkhalaf1 <87567854+mahmoudkhalaf1@users.noreply.github.com>
2021-09-15 01:17:29 +0200

    task7

---
+++ task7.php
@@ -0,0 +1,87 @@
+<!-- 
+    
+
+
+
+
+
+Admins: id   name email    password 
+        1
+
+        2
+
+        3
+
+
+
+Patients:   id     name     email      password
+            1      x        x@x.com    1
+
+            2      y        y@y.com    2
+
+            3      x        x@x.com   3
+
+
+
+
+
+Doctors :   id   name email   password      specialize   gender. time_id
+            1      x        x@x.com  1                           9
+
+            2      y        y@y.com  2                           10
+
+            3      x         x@x.com 3                           11
+
+
+
+specializeofdectors  pationt
+1                     m
+m                     1
+===================================
+
+m                     m
+
+
+
+specializes
+
+id      specialize(a )         
+
+1        ahmed                                         
+
+
+id  sspecialize(b )            
+
+1  ali                        
+
+
+id     specialize(c)  
+1      mai
+
+dateOfPationt
+
+id   time    doctors
+
+1    9         x
+
+2    10        y
+
+3     11         x
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ -->
